97abf54eecce9178f9114eeec12325d846edb2d1
Adel Shavalieva <adel.shavalieva@yandex.ru>
2021-12-07 15:41:18 +0300

    Add CORS query

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const routesUser = require('./routes/users');
 const routesCard = require('./routes/cards');
 const routesAuth = require('./routes/auth');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { NotFoundError } = require('./errors');
@@ -16,6 +17,45 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 }).then(() => console.log('Connection Successful'))
   .catch((err) => console.log(err));
 
+app.use(requestLogger); // подключаем логгер запросов
+
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://mesto.adel.nabiullina.nomoredomains.rocks',
+  'http://mesto.adel.nabiullina.nomoredomains.rocks',
+  'localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  // сохраняем список заголовков исходного запроса
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routesAuth);
@@ -23,7 +63,7 @@ app.use(auth, routesUser);
 app.use(auth, routesCard);
 app.use('*', () => { throw new NotFoundError('Ресурс не найден'); });
 
-const { PORT = 3000 } = process.env;
+app.use(errorLogger); // подключаем логгер ошибок
 
 // обработчики ошибок
 app.use(errors()); // обработчик ошибок celebrate
@@ -43,6 +83,4 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+module.exports = app;
